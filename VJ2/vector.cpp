b12f65347803cdfb9f9a8d662ed115541252a2d2
AntonioToni <antonio.gaming10@gmail.com>
2021-10-21 20:59:00 +0200

    Update vector.cpp

--- VJ2/vector.cpp
+++ VJ2/vector.cpp
@@ -2,37 +2,56 @@
 
 void MyVector::vector_new (size_t sz)
 {
-    /* TODO */
+    arr = new int[sz];
 }
 void MyVector::vector_delete()
 {
-    /* TODO */
+    delete[] arr;
 }
 void MyVector::vector_push_back(int n)
 {
-    /* TODO */
+    if (size == capacity)
+    {
+        int* newarr = arr;
+        arr = new int[capacity*=2];
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = newarr[i];
+        }
+        delete[] newarr;
+    }
+    arr[size] = n;
+    size++;
 }
 void MyVector::vector_pop_back()
 {
-    /* TODO */
+    size--;
 }
 int& MyVector::vector_front()
 {
-    /* TODO */
+    int front = arr[0];
+    return front;
 }
 int& MyVector::vector_back()
 {
-    /* TODO */
+    int back = arr[size-1];
+    return back;
 }
 size_t MyVector::vector_size() 
 {
-    /* TODO */
+    return size;
 }
 
+size_t MyVector::vector_capacity()
+{
+    return capacity;
+}
 
 void MyVector::print_vector()
 {
     for (size_t i = 0; i < vector_size(); ++i)
+    {
         std::cout << arr[i] << " ";
+    }
     std::cout << std::endl;
 }
