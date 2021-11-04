ad94a970fbafa1ddc93073367cb2e39a6efa1a87
AntonioToni <antonio.gaming10@gmail.com>
2021-11-04 17:08:04 +0100

    Add files via upload

---
+++ VJ3/Zad4.cpp
@@ -0,0 +1,23 @@
+#include "header.hpp"
+#include "Zad1funk.cpp"
+
+void del(vector<int> &v, vector<int>:: iterator i)
+{
+    v.erase(i);
+}
+
+int main()
+{
+    vector<int> v;
+    vector<int>::iterator i;
+    int n, x;
+	cout << "How many numbers do you want to enter? ";
+	cin >> n;
+	input(v, n);
+    output(v);
+    cout << "Which element of vector do you want to delete? ";
+    cin >> x;
+    i = v.begin() + x - 1;
+    del(v,i);
+    output(v);
+}
