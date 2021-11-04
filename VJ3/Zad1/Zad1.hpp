916e74598d7fac2fe40c6fa4058a2f8b69f421b8
AntonioToni <antonio.gaming10@gmail.com>
2021-11-04 11:43:12 +0100

    Add files via upload

---
+++ VJ3/Zad1/Zad1.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void input(vector<int> &v1, int n);
+void input2(vector<int> &v2, int a, int b);
+void output(vector<int> &v1);
+void output(vector<int> &v2);
