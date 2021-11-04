d272f563c04d3e6f9b55d3126d3a7c1e364a499a
AntonioToni <antonio.gaming10@gmail.com>
2021-11-04 18:27:18 +0100

    Add files via upload

---
+++ VJ3/Zad5.cpp
@@ -0,0 +1,25 @@
+#include "header.hpp"
+
+int substr(string str, string sub)
+{
+    int cnt = 0;
+    string::size_type pos = 0;
+    while(pos != string::npos)
+    {
+        cnt++;
+        pos=str.find(sub,pos+1);
+    }
+    return cnt;
+}
+
+int main()
+{
+    string str;
+    string sub;
+    cout << "Enter a string: ";
+    cin >> str;
+    cout << endl;
+    cout << "Enter a substring: ";
+    cin >> sub;
+    cout << substr(str,sub);
+}
