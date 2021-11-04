7cd10c6190b452a4f25a2fbef2ea3566a341d02b
AntonioToni <antonio.gaming10@gmail.com>
2021-11-04 12:44:24 +0100

    Add files via upload

---
+++ VJ3/Zad3.cpp
@@ -0,0 +1,22 @@
+#include "header.hpp"
+#include "zad1funk.cpp"
+
+int main()
+{
+	vector<int> v;
+    int n;
+    cout << "How many numbers do you want to enter? ";
+	cin >> n;
+    input(v, n);
+
+	sort(v.begin(), v.end());
+	v.insert(v.begin(), 0);
+    int sum = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		sum += v[i];
+	}
+	v.push_back(sum);
+	output(v);
+	system("pause");
+}
