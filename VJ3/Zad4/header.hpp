6f156a8bf20bff727c72e4964607af6266151461
AntonioToni <antonio.gaming10@gmail.com>
2021-11-15 17:04:45 +0100

    Create header.hpp

---
+++ VJ3/Zad4/header.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+void input(vector<int> &v1, int n);
+void input2(vector<int> &v2, int a, int b);
+void output(vector<int> &v1);
+void output(vector<int> &v2);
