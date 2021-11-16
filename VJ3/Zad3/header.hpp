98e5b3c96e007b701cd58b8c4e8fd5279f2fe538
AntonioToni <antonio.gaming10@gmail.com>
2021-11-16 20:51:13 +0100

    Deleted unnecessary code

--- VJ3/Zad3/header.hpp
+++ VJ3/Zad3/header.hpp
@@ -3,7 +3,5 @@
 #include <algorithm>
 
 using namespace std;
-void input(vector<int> &v1, int n);
-void input2(vector<int> &v2, int a, int b);
-void output(vector<int> &v1);
-void output(vector<int> &v2);
+void input(vector<int> &v, int n);
+void output(vector<int> &v);
